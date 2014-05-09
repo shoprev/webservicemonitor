b040015a0d9a74c32894942052a50cfdac0e0f55
shoprev <admin@shoprev.net>
2014-05-09 13:09:37 +0900

    v0.0.1

--- app.rb
+++ app.rb
@@ -9,13 +9,13 @@ if development?
   set :bind, '0.0.0.0'
 end
 
-set server: 'thin', connections: []
-set scheduler: Rufus::Scheduler
+# set server: 'thin', connections: []
+# set scheduler: Rufus::Scheduler
 
-logger.error "---------- config.yml s ----------"
-config_file './config.yml'
-settings.scheduler = Rufus::Scheduler.new
-logger.error "---------- config.yml e ----------"
+# logger.error "---------- config.yml s ----------"
+# config_file './config.yml'
+# settings.scheduler = Rufus::Scheduler.new
+# logger.error "---------- config.yml e ----------"
 
 # opt = { address:   settings.mail["address"],
 #         port:      settings.mail["port"],
