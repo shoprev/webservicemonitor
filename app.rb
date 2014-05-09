289da00b39b4f6bb2684902c1d8d8fed440fc98f
shoprev <admin@shoprev.net>
2014-05-09 13:40:41 +0900

    v0.0.1

--- app.rb
+++ app.rb
@@ -9,66 +9,66 @@ if development?
   set :bind, '0.0.0.0'
 end
 
-# set server: 'thin', connections: []
-# set scheduler: Rufus::Scheduler
+set server: 'thin', connections: []
+set scheduler: Rufus::Scheduler
 
-# logger.error "---------- config.yml s ----------"
-# config_file './config.yml'
-# settings.scheduler = Rufus::Scheduler.new
-# logger.error "---------- config.yml e ----------"
+logger.error "---------- config.yml s ----------"
+config_file './config.yml'
+settings.scheduler = Rufus::Scheduler.new
+logger.error "---------- config.yml e ----------"
 
-# opt = { address:   settings.mail["address"],
-#         port:      settings.mail["port"],
-#         domain:    settings.mail["domain"],
-#         user_name: settings.mail["user_name"],
-#         password:  settings.mail["password"]}
-# Mail.defaults do
-#   delivery_method :smtp, opt
-# end
+opt = { address:   settings.mail["address"],
+        port:      settings.mail["port"],
+        domain:    settings.mail["domain"],
+        user_name: settings.mail["user_name"],
+        password:  settings.mail["password"]}
+Mail.defaults do
+  delivery_method :smtp, opt
+end
 
-# def deliver_mail(h)
-#   Mail.deliver do
-#     from    Sinatra::Application.settings.mail["from"]
-#     to      Sinatra::Application.settings.mail["to"]
-#     subject "web service failure"
-#     body    "%s\n%s\n%s" % [h["url"],h["code"],h["time"]]
-#   end
-# end
+def deliver_mail(h)
+  Mail.deliver do
+    from    Sinatra::Application.settings.mail["from"]
+    to      Sinatra::Application.settings.mail["to"]
+    subject "web service failure"
+    body    "%s\n%s\n%s" % [h["url"],h["code"],h["time"]]
+  end
+end
 
-# def response_code(url)
-#   Net::HTTP.get_response(URI.parse(url)).code
-# end
+def response_code(url)
+  Net::HTTP.get_response(URI.parse(url)).code
+end
 
-# settings.scheduler.every settings.term do
-#   settings.urls.each.with_index(1) do |v,i|
-#     code = response_code(v)
-#     time = Time.now	
-#     deliver_mail({"url"=>v,"code"=>code,"time"=>time}) unless code == "200"
-#     settings.connections.each do |out|
-#       out << "data: {\"no\":\"#{i}\",\"url\":\"#{v}\",\"code\":\"#{code}\",\"time\":\"#{time}\"}\n\n" 
-#     end
-#   end
-# end
+settings.scheduler.every settings.term do
+  settings.urls.each.with_index(1) do |v,i|
+    code = response_code(v)
+    time = Time.now	
+    deliver_mail({"url"=>v,"code"=>code,"time"=>time}) unless code == "200"
+    settings.connections.each do |out|
+      out << "data: {\"no\":\"#{i}\",\"url\":\"#{v}\",\"code\":\"#{code}\",\"time\":\"#{time}\"}\n\n" 
+    end
+  end
+end
 
-# get '/stream', provides: 'text/event-stream' do
-#   response.headers['X-Accel-Buffering'] = 'no' # Disable buffering for nginx
-#   stream :keep_open do |out|
-#     settings.connections << out
-#     out.callback { settings.connections.delete(out) }
-#   end
-# end
+get '/stream', provides: 'text/event-stream' do
+  response.headers['X-Accel-Buffering'] = 'no' # Disable buffering for nginx
+  stream :keep_open do |out|
+    settings.connections << out
+    out.callback { settings.connections.delete(out) }
+  end
+end
 
 get '/' do
   logger.error "---------- / ----------"
   html=""
-  # settings.urls.each.with_index(1) do |v,i|
-  #   code = response_code(v)
-  #   time = Time.now	
-  #   deliver_mail({"url"=>v,"code"=>code,"time"=>time}) unless code == "200"
-  #   logger.error "---------- #{code} ----------"
-  #   html<< "<tr id=\"no#{i}\" class=\"#{code == "200" ? "default" : "warning"}\">
-  #   <td>#{i}</td><td><a href=\"#{v}\" target=\"_blank\">#{v.sub(/http(s):\/\//,"")}</a></td>
-  #   <td id=\"code#{i}\">#{code}</td><td id=\"time#{i}\">#{time}</td></tr>"
-  # end
+  settings.urls.each.with_index(1) do |v,i|
+    code = response_code(v)
+    time = Time.now	
+    deliver_mail({"url"=>v,"code"=>code,"time"=>time}) unless code == "200"
+    logger.error "---------- #{code} ----------"
+    html<< "<tr id=\"no#{i}\" class=\"#{code == "200" ? "default" : "warning"}\">
+    <td>#{i}</td><td><a href=\"#{v}\" target=\"_blank\">#{v.sub(/http(s):\/\//,"")}</a></td>
+    <td id=\"code#{i}\">#{code}</td><td id=\"time#{i}\">#{time}</td></tr>"
+  end
   erb :index, locals: { html: html }
 end
